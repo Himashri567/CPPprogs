92c7cff8aab163fef2112a2a9fab61f12a509120
Himashri567 <79900978+Himashri567@users.noreply.github.com>
2021-04-13 09:48:45 +0530

    Created Product.cpp
    To find the product of two numbers entered by users.

---
+++ Product.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+       int main()
+       {
+            double a,b,p;
+            cout<<"Enter two numbers:"<<endl;
+            cin>>a;
+            cin>>b;
+            p=a*b;
+            cout<<"Product="<<p<<endl;  
+               return 0;
+       }
