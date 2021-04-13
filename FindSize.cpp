2fc6a2b100dda6e8e2ffb24b0241ef1e161a1935
Himashri567 <79900978+Himashri567@users.noreply.github.com>
2021-04-13 09:32:10 +0530

    Creay

---
+++ FindSize.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+       int main()
+       {
+               int a,b,c,d;
+               a=sizeof(char);
+               b=sizeof(int);
+               c=sizeof(float);
+               d=sizeof(double);
+               cout<<"Size of char:"<<a<<"bytes"<<endl;
+               cout<<"Size of int:"<<b<<"bytes"<<endl;
+               cout<<"Size of float:"<<c<<"bytes"<<endl;
+               cout<<"Size of double:"<<d<<"bytes"<<endl;
+               return 0;
+       }
