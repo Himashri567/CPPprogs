aa0793545d86f292d80e7ae62ad2475b1572f3b8
Himashri567 <79900978+Himashri567@users.noreply.github.com>
2021-04-24 20:36:58 +0530

    Create time.cpp
    Enter time in hr:min:sec format and display the total time in seconds.

---
+++ time.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+void calculate(int,int,int);
+void displaysec(int);
+int main()
+{
+	int h,m,s;
+	cout<<"Hours?"<<endl;
+	cin>>h;
+	cout<<"Minutes?"<<endl;
+	cin>>m;
+	cout<<"Seconds?"<<endl;
+	cin>>s;
+	cout<<"The time is= "<<h<<":"<<m<<":"<<s<<endl;
+	calculate(h,m,s);
+	return 0;
+}
+void calculate(int hrs, int min, int sec)
+{
+	int tot;
+	tot=(60*60*hrs)+(60*min)+sec;
+	cout<<"Time in total seconds:"<<tot;
+}
